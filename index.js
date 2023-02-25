b48cc5417f2a0b24477807cae00bcc70df9d08af
Ishi Tran <berugen1st@gmail.com>
2023-02-25 12:28:15 +0700

    refactor and optimize performance

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const process = require("process");
 const crypto = require("crypto");
 const path = require("path");
 const fs = require("fs").promises;
+const { Transform } = require("stream");
+const { createReadStream } = require("fs");
 const { spawn } = require("child_process");
 
 const on = readline.createInterface({
@@ -12,19 +14,19 @@ const on = readline.createInterface({
 
 const readDirectory = async (dir) => {
   try {
-    console.log('-> Reading File...')
-
-    console.time('[Done Reading]: ')
-    const files = await fs.readdir(dir, { withFileTypes: false });
-    console.timeEnd('[Done Reading]: ')
+    console.log("-> Reading File...");
+    console.time("[Done Reading]: ");
+    const dirHandle = await fs.opendir(dir);
+    const files = [];
+    for await (const dirent of dirHandle)
+      if (dirent.isFile()) files.push(dirent.name);
+    console.timeEnd("[Done Reading]: ");
 
     console.log(`[Total File]: ${files.length}`);
+    console.log("\n");
 
-    console.log('\n')
-
-    console.log('-> Filtering File... ')
-
-    console.time('[Done Filtering]')
+    console.log("-> Filtering File... ");
+    console.time("[Done Filtering]");
     const filteredFiles = await Promise.all(
       files.map(async (file) => {
         const filePath = path.join(dir, file);
@@ -32,31 +34,36 @@ const readDirectory = async (dir) => {
         return stat.isFile() ? file : null;
       })
     );
-    console.timeEnd('[Done Filtering]')
-    console.log('\n')
+    console.timeEnd("[Done Filtering]");
+    console.log("\n");
+
     return filteredFiles.filter(Boolean);
   } catch (err) {
     console.error("Error reading directory", err);
   }
 };
 
-const readHashId = async (file) => {
-  try {
-    const fileBuffer = await fs.readFile(file);
-    const hashId = crypto.createHash("sha256");
-    hashId.update(fileBuffer);
-    const hex = hashId.digest("hex");
-    return hex;
-  } catch (err) {
-    console.error(`Error reading hash of ${file}`, err);
-  }
+const readHash = (file) => {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash("sha256");
+    const transform = new Transform({
+      transform(chunk, encoding, callback) {
+        hash.update(chunk);
+        callback();
+      },
+      flush(callback) {
+        resolve(hash.digest("hex"));
+        callback();
+      },
+    });
+    createReadStream(file).on("error", reject).pipe(transform);
+  });
 };
 
 const storeDuplicated = (files) => {
-  console.log('-> Storing Duplicated Hash... ')
-
   const set = new Set();
-  console.time('[Done Storing]')
+  console.log("-> Storing Duplicated Hash... ");
+  console.time("[Done Storing]");
   const duplicates = files.filter((file) => {
     if (set.has(file.hash)) return true;
     else {
@@ -64,46 +71,46 @@ const storeDuplicated = (files) => {
       return false;
     }
   });
-  console.timeEnd('[Done Storing]')
-
+  console.timeEnd("[Done Storing]");
   console.log(`[Total Duplicated]: ${duplicates.length}`);
-  console.log('\n')
+  console.log("\n");
   return duplicates;
 };
 
 const compareHash = (originalFileHashes, storedFileHashes) => {
   const set = new Set();
-
-  console.log('-> Re-Storing Duplicated Hash... ')
-
-  console.time('[Done Re-Storing]')
+  console.log("-> Re-Storing Duplicated Hash... ");
+  console.time("[Done Re-Storing]");
   for (const file of originalFileHashes) set.add(file.hash);
-  console.timeEnd('[Done Re-Storing]')
+  console.timeEnd("[Done Re-Storing]");
 
-  console.log('-> Comparing Hash... ')
-
-  console.time('[Done Comparing]')
+  console.log("-> Comparing Hash... ");
+  console.time("[Done Comparing]");
   const files = storedFileHashes.filter((file) => set.has(file.hash));
-  console.timeEnd('[Done Comparing]')
-  console.table(files)
+  console.timeEnd("[Done Comparing]");
 
-  console.log('\n')
+  console.table(files);
+  console.log("\n");
   return files;
 };
 
 const transferDuplicated = async (files, origin, dest) => {
-  console.log('-> Making directory... ')
-  await fs.mkdir(dest, { recursive: true });
+  try {
+    console.log("-> Making directory... ");
+    await fs.mkdir(dest, { recursive: true });
 
-  console.log('-> Transfering... ')
-  console.time('[Done Transfering]')
-  const renamePromises = files.map((file) =>
-    fs.rename(`${origin}/${file.name}`, `${dest}/${file.name}`)
-  );
-  await Promise.all(renamePromises);
-  console.timeEnd('[Done Transfering]')
+    console.log("-> Transfering... ");
+    console.time("[Done Transfering]");
+    const renamePromises = files.map((file) =>
+      fs.rename(`${origin}/${file.name}`, `${dest}/${file.name}`)
+    );
+    await Promise.all(renamePromises);
+    console.timeEnd("[Done Transfering]");
 
-  console.log('\n')
+    console.log("\n");
+  } catch (err) {
+    console.error("Error transfering file to directory", err);
+  }
 };
 
 const viewInFolder = (dest) => {
@@ -123,25 +130,24 @@ const run = async () => {
   const dir = await new Promise((resolve) =>
     on.question("Enter the directory: ", (answer) => resolve(answer))
   );
-  console.log('\n')
+  console.log("\n");
 
   const hashes = [];
   const files = await readDirectory(dir);
 
-  console.log('-> Reading Hash of File... ')
-
-  console.time('[Done Reading]')
-  for await (const file of files) {
+  console.log("-> Reading File Hash... ");
+  console.time("[Done Reading]");
+  for (const file of files) {
     if (file.startsWith(".")) continue;
     if (file.endsWith(".mp4")) continue;
     if (file.endsWith(".mkv")) continue;
-    const fromDirectory = path.join(dir, file);
-    const hex = await readHashId(fromDirectory);
+    const filesFromDirectory = path.join(dir, file);
+    const hex = await readHash(filesFromDirectory);
     hashes.push({ name: file, hash: hex });
   }
-  console.timeEnd('[Done Reading]')
-  console.table(hashes)
-  console.log('\n')
+  console.timeEnd("[Done Reading]");
+  console.table(hashes);
+  console.log("\n");
 
   const duplicates = storeDuplicated(hashes);
   if (duplicates.length === 0) process.exit();
